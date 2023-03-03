277d42d71349830d86453204f7b1d9e0466a4cb3
islamka2 <islamkaawad@gmail.com>
2023-03-03 10:50:38 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+$('.carousel').slick({
+    infinite: true,
+    prevArrow: '<img src="images/Arrow_left.svg" alt="">',
+    nextArrow: '<img src="images/Arrow_right.svg" alt="">',
+    slidesToShow: 3,
+    slidesToScroll: 3
+  });
